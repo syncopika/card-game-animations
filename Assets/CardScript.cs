efbadd8f601b3cad6b912a171f4a74f41ed0604f
nicholas hung <nhung93@outlook.com>
2021-07-10 22:12:58 -0400

    first commit

---
+++ Assets/CardScript.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardScript : MonoBehaviour
+{
+    //private Transform card;
+    private string cardName = "card";
+
+    private Vector3 endPosition;
+    private Vector3 startPosition;
+
+    private float speed = 9.0f;
+    private float arcHeight = -0.7f;
+    private float xRotationAngle = 0.0f; // in radians
+
+    private bool isMoving = false;
+    private bool isRotating = false;
+    private bool doFlip = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isMoving)
+        {
+            // since the cards start at different heights but they end up at the same height, we want a bit of an arc?
+            // https://luminaryapps.com/blog/arcing-projectiles-in-unity/
+            if (transform.position.x < endPosition.x)
+            {
+                float xDistance = Math.Abs(endPosition.x - startPosition.x);
+                float yDistance = Math.Abs(endPosition.y - startPosition.y);
+                float nextX = Mathf.MoveTowards(transform.position.x, endPosition.x, speed * Time.deltaTime);
+                float nextY = Mathf.MoveTowards(transform.position.y, endPosition.y, speed * Time.deltaTime * (yDistance / xDistance));
+                float baseZ = Mathf.Lerp(transform.position.z, endPosition.z, (nextX - startPosition.x) / xDistance);
+                float arc = arcHeight * (nextX - startPosition.x) * (nextX - endPosition.x) / (-0.25f * xDistance * xDistance);
+                transform.position = new Vector3(nextX, nextY, baseZ + arc);
+            }
+            else
+            {
+                transform.position = new Vector3(endPosition.x, endPosition.y, endPosition.z);
+                isMoving = false;
+            }
+        }
+
+        if (isRotating && doFlip)
+        {
+            if (transform.eulerAngles.y < 180)
+            {
+                // flip the card across the y-axis (so it goes from face-up to face-down or vice-versa)
+                // we start at eulerAngles.y being 0 deg
+                float angle = (2*Mathf.PI) / (endPosition.x - startPosition.x); // each card has a different rate of rotation when being flipped based on distance from the deck
+
+                // by rotating about the x-axis when rotating about the y-axis we can affect the z-axis to get my desired effect. discovered this accidentally lol
+                // this might be helpful in understanding why: https://www.reddit.com/r/Unity3D/comments/k03d4s/why_transformrotatex_y_0_also_rotates_zaxis/
+                transform.Rotate(xRotationAngle, angle * speed, 0);
+            }
+            else
+            {
+                // keep this rotation but ensure card is fully flipped across y-axis
+                transform.eulerAngles = new Vector3(transform.eulerAngles.x, 180, transform.eulerAngles.z);
+                isRotating = false;
+            }
+        }
+
+        if(isRotating && !doFlip)
+        {
+            float deg = (180 * xRotationAngle) / (float)Math.PI;
+
+            // kinda weird but this time since we're not rotating about y, we can just rotate about z.
+            if (isMoving)
+            {
+                transform.eulerAngles = Vector3.Lerp(transform.rotation.eulerAngles, new Vector3(0, 0, deg), Time.deltaTime);
+            }
+            else
+            {
+                isRotating = false;
+            }
+        }
+
+    }
+
+    public void setStartPosition(Vector3 start)
+    {
+        transform.position = start;
+        startPosition = new Vector3(start.x, start.y, start.z);
+    }
+
+    public void setEndPosition(Vector3 end)
+    {
+        endPosition = end;
+    }
+
+    public void setXRotationAngle(float angle)
+    {
+        xRotationAngle = angle;
+    }
+
+    public void toggleFlip(bool flip)
+    {
+        doFlip = flip;
+    }
+
+    public void setName(string name)
+    {
+        cardName = name;
+    }
+
+    public void placeCard()
+    {
+        isMoving = true;
+        isRotating = true;
+    }
+
+    public void flipCard()
+    {
+        // TODO
+    }
+}
---
+++ Assets/GameScript.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScript : MonoBehaviour
+{
+
+    public GameObject cardPrefab;
+
+    private List<GameObject> deck = new List<GameObject>();
+
+    private void createDeck(int numCards, Vector3 deckLocation)
+    {
+        for (int i = 0; i < numCards; i++)
+        {
+            // make sure the cards are stacked (so change the z-value for each card)
+            float newZ = deckLocation.z - (i+1)*0.05f;
+            Vector3 startPos = new Vector3(deckLocation.x, deckLocation.y, newZ);
+            GameObject card = Instantiate(cardPrefab, startPos, Quaternion.Euler(0, 0, 0));
+            card.AddComponent<CardScript>();
+            card.GetComponent<CardScript>().setStartPosition(startPos);
+
+            this.deck.Add(card);
+        }
+    }
+
+    private void setCardEndPositions(string configuration)
+    {
+        // TODO: pass in an enum representing some kind of pattern for the cards to end up in?
+        // default can be some m x n configuration? have different parameters?
+
+        int rowLength = 5; // place cards in rows of 5
+        int numRows = (int)Math.Ceiling(this.deck.Count / (float)rowLength);
+
+        if (configuration == "rows")
+        {
+            for (int i = 0; i < numRows; i++)
+            {
+                int offsetFromDeck = -6;
+                int newX = offsetFromDeck;
+                int newY = 3 - (i * 2);
+
+                for (int j = i * rowLength; j < Math.Min(i * rowLength + rowLength, this.deck.Count); j++)
+                {
+                    newX += 2; // kinda need to know the width of a card??
+                    CardScript script = this.deck[j].GetComponent<CardScript>();
+                    script.setEndPosition(new Vector3(newX, newY, -2));
+                    script.toggleFlip(false);
+                }
+            }
+        }else if(configuration == "splayed")
+        {
+            for (int i = 0; i < numRows; i++)
+            {
+                int offsetFromDeck = -6;
+                float newX = offsetFromDeck;
+                int newY = 3 - (int)(i * 1.2);
+                for (int j = i * rowLength; j < Math.Min(i * rowLength + rowLength, this.deck.Count); j++)
+                {
+                    newX += 2.2f;
+                    CardScript script = this.deck[j].GetComponent<CardScript>();
+                    script.setEndPosition(new Vector3(newX, newY, -2));
+
+                    // we're taking advantage of the different rates of y-rotation and rotating about x
+                    // as well to produce a varying skewed placement of each card about the z-axis.
+                    script.setXRotationAngle((float)Math.PI/3);
+
+                    script.toggleFlip(false);
+                }
+            }
+        }
+        else if (configuration == "splayed-symmetrical")
+        {
+            // cards should be angled like so: / -> | -> \
+            // figure out max and min angles (about the z-axis) that the cards should be at
+            // how many for each row, how much to space out
+            // for each subsequent row, stack on preceding row? just slightly moving towards the center each row (so in each subsequent row the cards will look closer together)
+            for (int i = 0; i < numRows; i++)
+            {
+                int offsetFromDeck = -6;
+                float newX = offsetFromDeck;
+                int newY = 3 - (int)(i * 1.2);
+                for (int j = i * rowLength; j < Math.Min(i * rowLength + rowLength, this.deck.Count); j++)
+                {
+                    newX += 2.2f;
+                    CardScript script = this.deck[j].GetComponent<CardScript>();
+                    script.setEndPosition(new Vector3(newX, newY, -2));
+
+                    // TODO
+                }
+            }
+        }
+    }
+
+    IEnumerator placeCard()
+    {
+        // notice we start backwards because we want to start at the top of the deck first
+        for(int i = this.deck.Count - 1; i >= 0; i--)
+        {
+            this.deck[i].GetComponent<CardScript>().placeCard();
+
+            // add a delay between each card
+            yield return new WaitForSeconds(0.2f);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // create a deck of cards
+        createDeck(15, new Vector3(-7, 3, -2));
+        setCardEndPositions("splayed");
+        StartCoroutine("placeCard");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
